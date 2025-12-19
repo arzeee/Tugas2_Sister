af70529d6538ed5a4b39f74e70854425f34d4942
arzeee <146150750+arzeee@users.noreply.github.com>
2025-12-19 19:40:22 +0700

    Update run.sh

--- synchronization/mutex/node3/run.sh
+++ synchronization/mutex/node3/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# Script untuk Node 3 (192.168.0.13)
 
-python3 ./kv.py --id 3 --tcp 8003 --udp 8103 --peers 127.0.0.1:8001=1,127.0.0.1:8002=2 --logger-addr 127.0.0.1:9000 --numnodes 3 --use-mutex 0
+USE_MUTEX=0
+
+echo "=== Menjalankan Node 3 (Tanpa Mutex) ==="
+python3 ../program/kv.py \
+  --id 3 \
+  --tcp 8000 \
+  --udp 8100 \
+  --peers 192.168.0.11:8000=1,192.168.0.12:8000=2 \
+  --logger-addr 192.168.0.10:9000 \
+  --numnodes 3 \
+  --use-mutex $USE_MUTEX
