c895b3547a028b37ecbd946b3b8f699ec9c573ff
arzeee <146150750+arzeee@users.noreply.github.com>
2025-12-19 19:39:14 +0700

    Update run.sh

--- synchronization/mutex/logger/run.sh
+++ synchronization/mutex/logger/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-python3 ./kv.py  --logger --logger-tcp 9000 --numnodes 3
+echo "=== LOGGER SERVER (Port 9000) ==="
+# Pastikan path ke kv.py benar.
+# Jika kv.py ada di folder ../program/, pakai ini:
+python3 ../program/kv.py --logger --logger-tcp 9000 --numnodes 3
